adbccb9dc3faaff411c2112d4ac203f983a82896
Brian Caball <tolcreator@yahoo.co.uk>
2015-08-17 21:39:42 +0100

    Added exploding dice, added test.

--- dice.py
+++ dice.py
@@ -7,6 +7,18 @@ def roll(numdice, sides):
         total += random.randint(1, sides)
     return total
 
+def explode(numdice, sides):
+    random.seed()
+    total = 0
+    for x in range(numdice):
+        done = False
+        while not done:
+            roll = random.randint(1, sides)
+            if not roll == sides:
+                done = True
+            total = total + roll
+    return total
+
 class Dice:
     def __init__(self):
         random.seed()
@@ -16,3 +28,14 @@ class Dice:
         for x in range(numdice):
             total += random.randint(1, sides)
         return total
+
+def test(sides, times):
+    gauss = {}
+    for x in range(times):
+        r = explode(1, sides)
+        if r in gauss.keys():
+            gauss[r] = gauss[r] + 1
+        else:
+            gauss[r] = 1
+    for key in sorted(gauss.keys()):
+        print "%02d: %d" % (key, gauss[key])
